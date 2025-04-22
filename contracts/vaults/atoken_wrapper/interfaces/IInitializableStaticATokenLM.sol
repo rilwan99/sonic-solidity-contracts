307231b962a39dc02be99281c974a3e3322e7eeb
David Zhang <david.d.zhang@gmail.com>
2025-04-23 00:05:20 +0700

    Update solidity versions and imports

--- contracts/vaults/atoken_wrapper/interfaces/IInitializableStaticATokenLM.sol
+++ contracts/vaults/atoken_wrapper/interfaces/IInitializableStaticATokenLM.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: agpl-3.0
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.20;
 
-import {IPool} from 'aave-v3-core/contracts/interfaces/IPool.sol';
-import {IAaveIncentivesController} from 'aave-v3-core/contracts/interfaces/IAaveIncentivesController.sol';
+import {IPool} from "../../../dlend/core/interfaces/IPool.sol";
+import {IAaveIncentivesController} from "../../../dlend/core/interfaces/IAaveIncentivesController.sol";
 
 /**
  * @title IInitializableStaticATokenLM
@@ -10,23 +10,27 @@ import {IAaveIncentivesController} from 'aave-v3-core/contracts/interfaces/IAave
  * @author Aave
  **/
 interface IInitializableStaticATokenLM {
-  /**
-   * @dev Emitted when a StaticATokenLM is initialized
-   * @param aToken The address of the underlying aToken (aWETH)
-   * @param staticATokenName The name of the Static aToken
-   * @param staticATokenSymbol The symbol of the Static aToken
-   **/
-  event Initialized(address indexed aToken, string staticATokenName, string staticATokenSymbol);
+    /**
+     * @dev Emitted when a StaticATokenLM is initialized
+     * @param aToken The address of the underlying aToken (aWETH)
+     * @param staticATokenName The name of the Static aToken
+     * @param staticATokenSymbol The symbol of the Static aToken
+     **/
+    event Initialized(
+        address indexed aToken,
+        string staticATokenName,
+        string staticATokenSymbol
+    );
 
-  /**
-   * @dev Initializes the StaticATokenLM
-   * @param aToken The address of the underlying aToken (aWETH)
-   * @param staticATokenName The name of the Static aToken
-   * @param staticATokenSymbol The symbol of the Static aToken
-   */
-  function initialize(
-    address aToken,
-    string calldata staticATokenName,
-    string calldata staticATokenSymbol
-  ) external;
+    /**
+     * @dev Initializes the StaticATokenLM
+     * @param aToken The address of the underlying aToken (aWETH)
+     * @param staticATokenName The name of the Static aToken
+     * @param staticATokenSymbol The symbol of the Static aToken
+     */
+    function initialize(
+        address aToken,
+        string calldata staticATokenName,
+        string calldata staticATokenSymbol
+    ) external;
 }
